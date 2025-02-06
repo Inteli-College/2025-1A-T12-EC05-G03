d33ada49983771bf1a3a75f98bf1b1a6123e1546
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2025-02-06 18:06:17 -0300

    fix: retira nodemodules

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/inteli/ProjetosInteli/2025-1A-T12-EC05-G03/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/inteli/ProjetosInteli/2025-1A-T12-EC05-G03/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/inteli/ProjetosInteli/2025-1A-T12-EC05-G03/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/inteli/ProjetosInteli/2025-1A-T12-EC05-G03/docs/src/css/custom.css"),
-];
