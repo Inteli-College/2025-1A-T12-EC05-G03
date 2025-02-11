263bce84c533075da1689c6cc0588bf6df3214cf
Ian Pereira Simão <159059598+ianpsa@users.noreply.github.com>
2025-02-11 17:18:14 -0300

    doc: adiciona personalização do css do docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -101,7 +101,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} NDC, todos os direitos reservados.`,
       },
       prism: {
         theme: prismThemes.github,
