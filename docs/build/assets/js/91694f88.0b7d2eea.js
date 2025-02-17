930245c180bbe4595dde63009f519c2c4c8521d0
Ian Pereira Simão <159059598+ianpsa@users.noreply.github.com>
2025-02-16 21:40:33 -0300

    adiciona alterações necessárias para o deploy

--- docs/build/assets/js/91694f88.0b7d2eea.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocsNDC=self.webpackChunkdocsNDC||[]).push([[80],{2080:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/build/assets/js/bc715e6a.56ae685d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocsNDC=self.webpackChunkdocsNDC||[]).push([[916],{590:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
