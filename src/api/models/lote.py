9cb3e4c9ec150ffeb8df8ada1d17f9b54112a4e1
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2025-03-13 13:37:11 -0300

    feat: implementa nova modelagem da tabela de remedio e lote

--- src/api/models/lote.py
+++ src/api/models/lote.py
@@ -8,9 +8,14 @@ class Lote(db.Model):
 
     # Definindo as colunas
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    num_lote = db.Column(db.String(100), nullable=False, primary_key=True)
+    num_lote = db.Column(db.String(100), nullable=False)
     data_validade = db.Column(db.DateTime, nullable=True)
     fabricante = db.Column(db.String(255), nullable=False)
+    id_remedio = db.Column(db.Integer, db.ForeignKey('remedio.id'), nullable=False)
+    quantidade = db.Column(db.Integer, nullable=False)
+
+    remedio_lote = db.relationship('Remedio', backref=db.backref('lotes', lazy=True))
+
 
     # Função para transformar em json
     def as_dict(self):
@@ -18,5 +23,7 @@ class Lote(db.Model):
             'id': self.id,
             'num_lote': self.num_lote,
             'data_validade': self.data_validade,
-            'fabricante': self.fabricante
+            'fabricante': self.fabricante,
+            'id_remedio': self.id_remedio,
+            'quantidade': self.quantidade
         }
